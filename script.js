554a4f862865a0e0e760b5b4fa1f9ce373f51ffb
Glitch (glitch-blank-website) <none>
2022-03-12 19:50:15 +0000

    🏎⛸ Checkpoint ./style.css:43022770/881 ./index.html:43022770/9302 ./script.js:43022770/163

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-console.log("Hello, world!");
+var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+
+var progress=0;
+
+var gamePlaying
