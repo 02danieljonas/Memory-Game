35e8f4e091351d27e7e994c9ec1037bffc914eec
Glitch (glitch-blank-website) <none>
2022-03-12 20:02:13 +0000

    📚🐆 Checkpoint ./script.js:43022770/1396 ./index.html:43022770/45

--- script.js
+++ script.js
@@ -3,6 +3,56 @@
 
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 
-var progress=0;
+var progress = 0;
 
-var gamePlaying
+var gamePlaying = false;
+
+var tonePlaying = false;
+
+var volume = 0.5;
+
+function startGame(){
+  progress = 0;
+  gamePlaying = true;
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  
+}
+
+function stopGame(){
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+}
+
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 466.2
+}
+
+function playTone(btn, len){
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+  context.resume()
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  }, len)
+     
+}
+
+function startTone(btn){
+  if(!tonePlaying){
+    context.resume()
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume, context.currentTime+0.05,0.025)
+    context.resume()
+    tonePlaying = true;
+  }
+}
+
+function stopTone(){
+  g.gain.setTargetAtTime(0, context.current)
+}
