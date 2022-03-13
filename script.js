50522222922d2db62f262ae7b80f29601dfa40d4
Glitch (glitch-blank-website) <none>
2022-03-13 08:28:04 +0000

    🔦🚗 Checkpoint ./script.js:43022770/7726 ./style.css:43022770/1421

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
+//https://www.w3schools.com/js/js_random.asp
+patternLength = 10
+for (let i = 0; i < patternLength; i++) {
+  Math.floor(Math.random() * 4) + 1;
+}
+
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0;
 var gamePlaying = false;
@@ -11,29 +17,28 @@ var guessCounter = 0;
 // TODO: fix glitch dragging mouse away from screen causes sounds to continue until any button is pressed
 //TODO: fix glitch where if you know the pattern before hand sound will play on top of each other
 
-var AudioContext = window.AudioContext || window.webkitAudioContext
-var context = new AudioContext()
-var o = context.createOscillator()
-var g = context.createGain()
-g.connect(context.destination)
-g.gain.setValueAtTime(0, context.currentTime)
-o.connect(g)
-o.start(0)
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var context = new AudioContext();
+var o = context.createOscillator();
+var g = context.createGain();
+g.connect(context.destination);
+g.gain.setValueAtTime(0, context.currentTime);
+o.connect(g);
+o.start(0);
 
 const clueHoldTime = 1000; //how long each clue is played for
 const cluePauseTime = 333; //how long to pause between clues
 const nextClueWaitTime = 1000; //how long to wait before next clue starts
 
-
-function startGame(){
+function startGame() {
   progress = 0;
   gamePlaying = true;
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
-  playClueSequence(); 
+  playClueSequence();
 }
 
-function stopGame(){
+function stopGame() {
   gamePlaying = false;
   document.getElementById("startBtn").classList.remove("hidden");
   document.getElementById("stopBtn").classList.add("hidden");
@@ -43,116 +48,92 @@ const freqMap = {
   1: 261.6,
   2: 329.6,
   3: 392,
-  4: 466.2
-}
+  4: 466.2,
+};
 
-function playTone(btn, len){
-  o.frequency.value = freqMap[btn]
+function playTone(btn, len) {
+  o.frequency.value = freqMap[btn];
   g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
-  context.resume()
-  tonePlaying = true
-  setTimeout(function(){
-    stopTone()
-  }, len)
-     
+  context.resume();
+  tonePlaying = true;
+  setTimeout(function () {
+    stopTone();
+  }, len);
 }
 
-function startTone(btn){
-  if(!tonePlaying){
-    context.resume()
-    o.frequency.value = freqMap[btn]
-    g.gain.setTargetAtTime(volume, context.currentTime+0.05,0.025)
-    context.resume()
+function startTone(btn) {
+  if (!tonePlaying) {
+    context.resume();
+    o.frequency.value = freqMap[btn];
+    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+    context.resume();
     tonePlaying = true;
   }
 }
 
-function stopTone(){
-  g.gain.setTargetAtTime(0, context.currentTime+0.05, 0.025)
-  tonePlaying = false
+function stopTone() {
+  g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025);
+  tonePlaying = false;
 }
 
-function lightButton(btn){
-  document.getElementById("button"+btn).classList.add("lit")
+function lightButton(btn) {
+  document.getElementById("button" + btn).classList.add("lit");
 }
 
-function clearButton(btn){
-  document.getElementById("button"+btn).classList.remove("lit")
+function clearButton(btn) {
+  document.getElementById("button" + btn).classList.remove("lit");
 }
 
-function playSingleClue(btn){
-  if(gamePlaying){
+function playSingleClue(btn) {
+  if (gamePlaying) {
     lightButton(btn);
-    playTone(btn,clueHoldTime);
-    setTimeout(clearButton,clueHoldTime,btn);
+    playTone(btn, clueHoldTime);
+    setTimeout(clearButton, clueHoldTime, btn);
   }
 }
 
-function playClueSequence(){
+function playClueSequence() {
   guessCounter = 0;
-  context.resume() //This code disappeared after I was told to write it
-  let delay = nextClueWaitTime
-  for(let i=0; i<=progress; i++){
-    console.log("Play single clue: " + pattern[i] + " in "+ delay + "ms")
-    setTimeout(playSingleClue,delay,pattern[i])
-    delay += clueHoldTime
-    delay += cluePauseTime
+  context.resume(); //This code disappeared after I was told to write it
+  let delay = nextClueWaitTime;
+  for (let i = 0; i <= progress; i++) {
+    console.log("Play single clue: " + pattern[i] + " in " + delay + "ms");
+    setTimeout(playSingleClue, delay, pattern[i]);
+    delay += clueHoldTime;
+    delay += cluePauseTime;
   }
 }
 
-function loseGame(){
-  stopGame()
-  alert("Game Over. You lost! \n Progress: "+progress)
+function loseGame() {
+  stopGame();
+  alert("Game Over. You lost! \n Progress: " + progress);
 }
 
-function  winGame(){
-  stopGame()
-  alert("Game Over. You Won!")
+function winGame() {
+  stopGame();
+  alert("Game Over. You Won!");
 }
 
-function guess(btn){
-  console.log("User guessed: "+btn)
-    
-  if(!gamePlaying){
+function guess(btn) {
+  console.log("User guessed: " + btn);
+
+  if (!gamePlaying) {
     return;
   }
-  
-/*  if(pattern[guessCounter] == btn){
-    if(guessCounter == progress){
-      if (guessCounter == pattern.length){
-        winGame()
-      }
-      else{
-        guessCounter++;
-        playClueSequence();
-      }
-    }
-    else {
-      guessCounter++;
-    }
-  }
-  else{
-    loseGame()
-  }
- */ 
-  if (!(pattern[guessCounter] == btn)){
-    console.log("return 1")
-    loseGame()
+  if (!(pattern[guessCounter] == btn)) {
+    loseGame();
     return;
   }
-  
-  if (!(guessCounter == progress)){
-    console.log("return 2")
+
+  if (!(guessCounter == progress)) {
     guessCounter++;
     return;
   }
-  
-  if (!(progress == pattern.length - 1)){
-    console.log("return 3")
 
+  if (!(progress == pattern.length - 1)) {
     progress++;
     playClueSequence();
     return;
   }
-  winGame()
+  winGame();
 }
