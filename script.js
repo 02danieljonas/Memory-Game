0012adb8cd31bd4c72a06cc8c4925f4fe37ab528
Glitch (glitch-blank-website) <none>
2022-03-17 20:10:29 +0000

    🚿🌦 Checkpoint ./script.js:43022770/1409 ./index.html:43022770/6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-//https://www.w3schools.com/js/js_random.asp, https://www.codegrepper.com/code-examples/javascript/how+to+append+empty+array+in+javascript, https://www.w3schools.com/howto/howto_js_rangeslider.asp, https://www.w3schools.com/cssref/default.asp (used for finding random things), https://stackoverflow.com/questions/4015345/how-do-i-properly-escape-quotes-inside-html-attributes, https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range, https://pietschsoft.com/post/2015/09/05/javascript-basics-how-to-create-a-dictionary-with-keyvalue-pairs, https://stackoverflow.com/questions/15189857/what-is-the-most-efficient-way-to-empty-a-plain-object-in-javascript, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+//https://www.w3schools.com/js/js_random.asp, https://www.codegrepper.com/code-examples/javascript/how+to+append+empty+array+in+javascript, https://www.w3schools.com/howto/howto_js_rangeslider.asp, https://www.w3schools.com/cssref/default.asp (used for finding random things), https://stackoverflow.com/questions/4015345/how-do-i-properly-escape-quotes-inside-html-attributes, https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range, https://pietschsoft.com/post/2015/09/05/javascript-basics-how-to-create-a-dictionary-with-keyvalue-pairs, https://stackoverflow.com/questions/15189857/what-is-the-most-efficient-way-to-empty-a-plain-object-in-javascript, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
 
 // TODO: add a configure button that allows you to configure the app with sliders
 var patternLength = 10;
@@ -24,15 +24,7 @@ var userGuessTime;
 
 
 
-//TODO: take the value of the button slider and connect it to actual buttons
 
-var settingsValue = {
-//   placeholderGameLen: 1,
-//   placeholderVolume: 1,
-//   placeholderLives: 1,
-//   placeholderButtons: 1,
-//   placeholderSequencee: 1,
-};
 
 function print(q) {
   console.log(q);
@@ -73,15 +65,14 @@ function stopGame() {
   document.getElementById("stopBtn").classList.add("hidden");
 }
 
-var freqMap = [
-];
-
-let temp=((500-260)/(buttonAmount-1))
-for (let i = 0; i < buttonAmount; i++) {
-  freqMap[i]=Math.round(260+(temp*i))
+var freqMap = [];
+populatFreqMap()
+function populatFreqMap(){
+  let temp=((500-260)/(buttonAmount-1))
+  for (let i = 0; i < buttonAmount; i++) {
+    freqMap[i]=Math.round(260+(temp*i))
+  }
 }
-
-
 
 function playTone(btn, len) {
   o.frequency.value = freqMap[btn];
@@ -204,11 +195,17 @@ function close(){
   document.getElementById("settings").innerText = "Settings";
 }
 
-
 function applySettings(){
   console.log(settingsValue)
   close() 
 }
+
+//TODO: take the value of the button slider and connect it to actual buttons
+
+var settingsValue = [-patternLength, -volume, -lives, -buttonAmount, -countDownTimer];
+
+if(Math.abs()
+
 
 
 function updateSliderPlaceholder(slider, placeholder){  
