4c634d38ea40b345000212d27ab60c19d5822911
Glitch (wry-tungsten-smartphone) <none>
2022-03-18 16:36:59 +0000

    💺😺 Checkpoint ./script.js:43022770/1293

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//https://www.w3schools.com/js/js_random.asp, https://www.codegrepper.com/code-examples/javascript/how+to+append+empty+array+in+javascript, https://www.w3schools.com/howto/howto_js_rangeslider.asp, https://www.w3schools.com/cssref/default.asp (used for finding random things), https://stackoverflow.com/questions/4015345/how-do-i-properly-escape-quotes-inside-html-attributes, https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range, https://pietschsoft.com/post/2015/09/05/javascript-basics-how-to-create-a-dictionary-with-keyvalue-pairs, https://stackoverflow.com/questions/15189857/what-is-the-most-efficient-way-to-empty-a-plain-object-in-javascript, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs, https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l
+//https://www.w3schools.com/js/js_random.asp, https://www.codegrepper.com/code-examples/javascript/how+to+append+empty+array+in+javascript, https://www.w3schools.com/howto/howto_js_rangeslider.asp, https://www.w3schools.com/cssref/default.asp (used for finding random things), https://stackoverflow.com/questions/4015345/how-do-i-properly-escape-quotes-inside-html-attributes, https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range, https://pietschsoft.com/post/2015/09/05/javascript-basics-how-to-create-a-dictionary-with-keyvalue-pairs, https://stackoverflow.com/questions/15189857/what-is-the-most-efficient-way-to-empty-a-plain-object-in-javascript, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs, https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l, https://www.samanthaming.com/tidbits/70-3-ways-to-clone-objects/,https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in, 
 
 var pattern = [];
 var clueInProgress = false;
@@ -18,6 +18,10 @@ var gameSettings = {
   lives: 3,
   buttonAmount: 4,
 };
+
+var userGameSettings = Object.assign({}, gameSettings);
+
+console.log(userGameSettings)
 
 // TODO: fix glitch dragging mouse away from screen causes sounds to continue until any button is pressed
 //TODO: fix glitch where if you know the pattern before hand sound will play on top of each other
@@ -190,10 +194,28 @@ function showSettingContainer() {
     document.getElementById("settingsContainer").classList.remove("hidden");
     document.getElementById("settings").innerText = "Cancel";
   } else {
-    //change the values of the sliders back to OG
-    close();
+    cancel()
   }
 }
+
+function cancel(){
+  for (const property in userGameSettings) {
+    if (userGameSettings[property] != gameSettings[property]){
+        document.getElementById(property).innerHTML = gameSettings[placeholder];
+        userGameSettings[property] == gameSettings[property])
+    }
+  }
+
+  for everything in UGS thats different in GS:
+  change slider info to GS
+  change UGS info to copy GS
+  
+  userGameSettings = Object.assign({}, gameSettings);
+  close();
+
+}
+
+
 
 function close() {
   document.getElementById("settingsContainer").classList.add("hidden");
@@ -201,13 +223,17 @@ function close() {
 }
 
 function applySettings() {
+  gameSettings = Object.assign({}, userGameSettings);
+
   console.log(gameSettings);
+  console.log(userGameSettings);
+
   close();
 }
 
 function updateSliderPlaceholder(slider, placeholder) {
   var sliderElem = document.getElementById(slider);
   var placeholderElem = document.getElementById(placeholder);
-  gameSettings[placeholder] = Number(sliderElem.value);
-  placeholderElem.innerHTML = gameSettings[placeholder];
+  userGameSettings[placeholder] = Number(sliderElem.value);
+  placeholderElem.innerHTML = userGameSettings[placeholder];
 }
