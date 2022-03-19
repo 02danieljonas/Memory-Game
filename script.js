df4c7267f0b8e04e77ca99ecd80371d97ba27776
Glitch (wry-tungsten-smartphone) <none>
2022-03-19 18:33:33 +0000

    🐵🍓 Checkpoint ./style.css:43022770/1 ./script.js:43022770/683 ./index.html:43022770/15

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 //https://www.w3schools.com/js/js_random.asp, https://www.codegrepper.com/code-examples/javascript/how+to+append+empty+array+in+javascript, https://www.w3schools.com/howto/howto_js_rangeslider.asp, https://www.w3schools.com/cssref/default.asp (used for finding random things), https://stackoverflow.com/questions/4015345/how-do-i-properly-escape-quotes-inside-html-attributes, https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range, https://pietschsoft.com/post/2015/09/05/javascript-basics-how-to-create-a-dictionary-with-keyvalue-pairs, https://stackoverflow.com/questions/15189857/what-is-the-most-efficient-way-to-empty-a-plain-object-in-javascript, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs, https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l, https://www.samanthaming.com/tidbits/70-3-ways-to-clone-objects/, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in, https://www.w3schools.com/css/css_border.asp,https://www.w3schools.com/jsref/jsref_now.asp, https://www.w3schools.com/cssref/tryit.asp?filename=trycss_position2, https://www.w3schools.com/cssref/pr_class_position.asp,
 
+/*
+What I have had issues with: 
+the error message not being able to switch from transparent and not
+player cheating and playing sound at the same time as simon is saying
+the range sliders and extracting values
+
+
+
+*/
 var pattern = []; //array contain the pattern for that round
 var clueInProgress = false; //is a clue playing right now
 var progress = 0; //Score of the player
@@ -70,7 +79,7 @@ function timer(clueLength) {
 }
 
 function startGame() {
-  error("Starting Game")
+  showErrorMessage("Starting Game")
   if (document.getElementById("settingsContainer").classList == "hidden") {
     //if settings is closed
     strikes = 0;
@@ -95,7 +104,7 @@ function startGame() {
 }
 
 function stopGame() {
-  error("Stopping Game")
+  showErrorMessage("Stopping Game")
 
   gamePlaying = false;
   document.getElementById("startBtn").classList.remove("hidden");
@@ -193,7 +202,7 @@ function guess(btn) {
   if (!gamePlaying) {
     return;
   } else if (!(Date.now() > validGuessTime)) {
-    error(`Please wait ${validGuessTime - Date.now()}ms to guess`);
+    showErrorMessage(`Please wait ${validGuessTime - Date.now()}ms to guess`);
     console.log(`Please wait ${validGuessTime - Date.now()}ms to guess`);
     return;
   } else if (!(pattern[guessCounter] == btn)) {
@@ -228,7 +237,7 @@ function showSettingContainer() {
       cancel();
     }
   } else {
-    error("Please stop the game to change settings");
+    showErrorMessage("Please stop the game to change settings");
     // console.log("Error");
   }
 }
@@ -288,11 +297,13 @@ function updateSliderPlaceholder(slider, placeholder) {
   placeholderElem.innerHTML = userGameSettings[placeholder];
 }
 
-function error(info) {
+function showErrorMessage(info) {
   document.getElementById("errorMessage").classList.remove("hidden");
 
   document.getElementById("errorMessage").innerHTML = info;
+                      
+  console.log("Why is this not working")
   
-  document.getElementById("errorMessage").classList.add("ehidden");
+  document.getElementById("errorMessage").classList.add("hidden");
 
 }
