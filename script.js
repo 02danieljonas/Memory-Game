64fb190664c4d51ef36ff6d387ea655e0301f476
Glitch (wry-tungsten-smartphone) <none>
2022-03-20 21:14:00 +0000

    👐👏 Checkpoint ./script.js:43022770/24822

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//https://www.w3schools.com/js/js_random.asp, https://www.codegrepper.com/code-examples/javascript/how+to+append+empty+array+in+javascript, https://www.w3schools.com/howto/howto_js_rangeslider.asp, https://www.w3schools.com/cssref/default.asp (used for finding random things), https://stackoverflow.com/questions/4015345/how-do-i-properly-escape-quotes-inside-html-attributes, https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range, https://pietschsoft.com/post/2015/09/05/javascript-basics-how-to-create-a-dictionary-with-keyvalue-pairs, https://stackoverflow.com/questions/15189857/what-is-the-most-efficient-way-to-empty-a-plain-object-in-javascript, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs, https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l, https://www.samanthaming.com/tidbits/70-3-ways-to-clone-objects/, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in, https://www.w3schools.com/css/css_border.asp,https://www.w3schools.com/jsref/jsref_now.asp, https://www.w3schools.com/cssref/tryit.asp?filename=trycss_position2, https://www.w3schools.com/cssref/pr_class_position.asp,
+//https://www.w3schools.com/js/js_random.asp, https://www.codegrepper.com/code-examples/javascript/how+to+append+empty+array+in+javascript, https://www.w3schools.com/howto/howto_js_rangeslider.asp, https://www.w3schools.com/cssref/default.asp (used for finding random things), https://stackoverflow.com/questions/4015345/how-do-i-properly-escape-quotes-inside-html-attributes, https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range, https://pietschsoft.com/post/2015/09/05/javascript-basics-how-to-create-a-dictionary-with-keyvalue-pairs, https://stackoverflow.com/questions/15189857/what-is-the-most-efficient-way-to-empty-a-plain-object-in-javascript, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs, https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l, https://www.samanthaming.com/tidbits/70-3-ways-to-clone-objects/, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in, https://www.w3schools.com/css/css_border.asp,https://www.w3schools.com/jsref/jsref_now.asp, https://www.w3schools.com/cssref/tryit.asp?filename=trycss_position2, https://www.w3schools.com/cssref/pr_class_position.asp, https://www.w3schools.com/js/js_cookies.asp
 
 /*
 What I have had issues with: 
@@ -35,6 +35,35 @@ var gameSettings = {
   timeDecay: 0,
 };
 
+//goes to cookies and changes the above values to what ever was in cookies
+
+saveCookie();
+
+loadCookie()
+console.log(gameSettings);
+
+function loadCookie() {
+  let x = document.cookie;
+  console.log(x);
+
+
+  //looks for my cookies
+  //if cookies load
+}
+
+function saveCookie() {
+  for (var key in gameSettings) {
+    var value = gameSettings[key];
+    document.cookie = key + "=" + value + ";" + ";path=/";
+  }
+}
+
+
+
+
+
+
+
 var userGameSettings = Object.assign({}, gameSettings); //clones gameSettings, UserGS is used in for the settings screen and if the user presses apply, it runs the function that applies it
 
 var freqMap;
@@ -69,18 +98,6 @@ function print(q) {
   console.log(q);
 }
 
-function loadCookie(){
-  //looks for my cookies
-  //if cookies load
-}
-
-function saveCookie(object){
-  //loops through the object and saves it to the cookie
-  var json = JSON.stringify(object)
-  
-}
-
-
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 var o = context.createOscillator();
@@ -97,7 +114,7 @@ var nextClueWaitTime = 1000; //how long to wait before next list of clues starts
 function timer(clueLength) {
   //if I had more time I would use setTimeout but I want to work on other features
   let howLong = nextClueWaitTime; //
-  howLong += ((clueLength + 1) * (cluePauseTime + clueHoldTime)-100); //for every clue add cPT and cHT to find out how long the clue plays for
+  howLong += (clueLength + 1) * (cluePauseTime + clueHoldTime) - 100; //for every clue add cPT and cHT to find out how long the clue plays for
   setTimeout(function () {
     console.log("Play now");
   }, howLong);
@@ -245,8 +262,8 @@ function guess(btn) {
     progress++;
     if (cluePauseTime < 150) {
       console.log(cluePauseTime);
-      cluePauseTime *= 0.80;
-      clueHoldTime *= 0.80;
+      cluePauseTime *= 0.8;
+      clueHoldTime *= 0.8;
     }
     playClueSequence();
     return;
@@ -314,10 +331,10 @@ function applySettings() {
     updateButtons(userGameSettings["buttonAmount"]);
   }
   gameSettings = Object.assign({}, userGameSettings);
-  
-  close();
-  document.cookie="username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
 
+  close();
+  document.cookie =
+    "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
 }
 
 function updateSliderPlaceholder(slider, placeholder) {
